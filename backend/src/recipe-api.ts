dfc37ed1c25c6b11a99294797c1bdf4a152d9b50
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-22 17:52:17 -0500

    Made schema.prisma

---
+++ backend/src/recipe-api.ts
@@ -0,0 +1,20 @@
+const apiKey = process.env.API_KEY;
+
+const searchRecipes = async (searchTerm: string, page: number) => {
+  if (!apiKey) {
+    throw new Error("API key not found!");
+  }
+
+  const url = new URL("https://api.spoonacular.com/recipes/complexSearch");
+  const queryParams = {
+    apiKey, //shorthand version because I don't want to do this => apiKey: apiKey
+    query: searchTerm,
+    number: "10",
+    offset: (page * 10).toString(), // Aligns results with page number
+  };
+  url.search = new URLSearchParams(queryParams).toString();
+
+  try {
+    const searchResponse = await fetch(url);
+  } catch (error) {}
+};
