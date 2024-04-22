283159c8b0ec50fe63e490fd86ef5d1ba4498c71
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-22 16:54:23 -0500

    Made 'api/recipes/search'

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/recipes/search", async (req, res) => {
+  res.json({ message: "Success!" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on localhost:3000");
+});
