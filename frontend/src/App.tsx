7d8ebc98137cde2d1fd5eb1f9d45bf4fe04c99d6
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-22 22:52:03 -0500

    Created base logic for mapping out the results of a searchTerm for the recipe and displaying it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,30 @@
-import react from "react";
+import "./App.css";
+import { useState } from "react";
+import * as api from "./api";
 
 const App = () => {
-  return <div>Hello from the other side! I'd really like a side of fries!</div>;
+  const [searchTerm, setSearchTerm] = useState("burgers and fries");
+  const [recipes, setRecipes] = useState([]);
+
+  const handleSearchSubmit = async () => {
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1);
+      setRecipes(recipes);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div>
+          recipe image location: {recipe.image}
+          recipe title: {recipe.title}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
